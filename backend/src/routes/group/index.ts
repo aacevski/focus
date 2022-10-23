fed5ba07eaead1f74e6f2ae8042a756d5088e79a
Aleksandra Zografska <aleksandra.zografska@tenforce.com>
2022-10-23 20:57:42 +0200

    add delete group

--- backend/src/routes/group/index.ts
+++ backend/src/routes/group/index.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 
 import createGroup from "../../controllers/group/create-group";
+import deleteGroup from "../../controllers/group/delete-group";
 import editGroup from "../../controllers/group/edit-group";
 import getAllGroups from "../../controllers/group/get-all-groups";
 
@@ -9,6 +10,9 @@ const router = Router();
 router.get("/", getAllGroups);
 
 router.post("/", createGroup);
+
 router.post("/:id", editGroup);
+
+router.delete("/:id", deleteGroup);
 
 export default router;
