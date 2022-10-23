c5ad3e7ec65e0bf6dd171ae126ee35a163f4c3b6
Aleksandra Zografska <aleksandra.zografska@tenforce.com>
2022-10-23 19:57:33 +0200

    add edit group

---
+++ backend/src/controllers/group/edit-group.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from "express";
+
+import dataSource from "../../data-source";
+import { Group } from "../../entity/group.entity";
+
+const createGroup = async (req: Request, res: Response) => {
+  const groupRepository = dataSource.getRepository(Group);
+
+  try {
+    const { id } = req.params;
+    const { title, content, owner } = req.body;
+
+    const group = await groupRepository.findOneByOrFail({
+      id: parseInt(id),
+    });
+
+    group.title = title;
+    group.content = content;
+    group.owner = owner;
+
+    res.json({
+      status: "success",
+      data: {
+        group,
+      },
+    });
+  } catch {
+    res.status(500).json({
+      error: "Internal server error",
+    });
+  }
+};
+
+export default createGroup;
--- backend/src/routes/group/index.ts
+++ backend/src/routes/group/index.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 
 import createGroup from "../../controllers/group/create-group";
+import editGroup from "../../controllers/group/edit-group";
 import getAllGroups from "../../controllers/group/get-all-groups";
 
 const router = Router();
@@ -8,5 +9,6 @@ const router = Router();
 router.get("/", getAllGroups);
 
 router.post("/", createGroup);
+router.post("/:id", editGroup);
 
 export default router;
